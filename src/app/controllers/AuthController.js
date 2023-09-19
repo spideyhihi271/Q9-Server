51eaf19dc645e0256de8ea0884c5bbbca3564534
Nguyen <pysanmeo@gmail.com>
2023-09-19 20:47:32 +0700

    replace bycrypt

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -1,5 +1,5 @@
 const { User, validate } = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const salt = bcrypt.genSaltSync(10);
 const tranform = require('../../utils/tranformMongoes');
